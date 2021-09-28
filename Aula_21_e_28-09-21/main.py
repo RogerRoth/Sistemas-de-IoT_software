5f295d8fdffb6115d0bde7853856547ff2852d64
Patrick <trickmartini@gmail.com>
2021-09-27 21:39:33 -0300

    criação arquivos

---
+++ Aula_21_e_28-09-21/main.py
@@ -0,0 +1,8 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://ghelfer:chimarrao@iotdb.asglx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.IoTdb
+collection = db.Sensores
+x = collection.find().sort("pressao")
+for i in x:
+    print(i)
